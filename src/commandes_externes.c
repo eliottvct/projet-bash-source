fc5c7c2b876727e36bc4abdec78e492fba5818c2
Eliott Vincent <eliott.20100@gmail.com>
2017-02-11 20:41:22 +0100

    "ls" command executs the custom commande_ls.c

--- src/commandes_externes.c
+++ src/commandes_externes.c
@@ -9,18 +9,17 @@
 #include "parse.h"
 
 t_bool ActionEXEC(parse_info *info, int debut, int nbArg) {
-    int status;
     pid_t pid_fils = -1;
-    t_bool result = faux;
 
     if(info->modificateur[2] != TUBE) {
         pid_fils = fork();
-        if (pid_fils == -1){
-            //TODO : Ajouter printf error
+        if (pid_fils == -1) {
+            printf("Une erreur a eu lieu : %s\n", strerror(errno));
             return faux;
-        }else if (pid_fils == 0){ //Fils
+        }
+        else if (pid_fils == 0) //Fils
             execute(info, nbArg, debut);
-        }else{
+        else {
             int status;
             if ((info->modificateur[debut + 1] != ARRIERE_PLAN)) {
                 waitpid(pid_fils,&status,0);
@@ -36,47 +35,49 @@ t_bool ActionEXEC(parse_info *info, int debut, int nbArg) {
                     printf("Le processus %d est mort: signal %d%s\n", pid_fils, WTERMSIG(status), WCOREDUMP(status) ? " - core dumped" : "");
                     return faux;
                 }
-                else {  //program exited abnormally
+                else//program exited abnormally
                     return faux;
-                }
             }
         }
-    }else{
-        execute(info, nbArg, debut);
     }
+    else
+        execute(info, nbArg, debut);
 }
 
 
 void execute(parse_info * info, int nbArg, int debut) {
     // On prépare la commande
-    char * args[ARG_MAX];
+    char *args[ARG_MAX];
 
     //TODO : if ligne_cmd debut != ls ou echo
     char *cmd = info->ligne_cmd[debut];
+
     if (!EST_EGAL(info->sortie, "")) {  //redirection demandée
-        int sortie = open(info->sortie, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH );
+        int sortie = open(info->sortie, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
         if (sortie != NULL) {   //si le fichier n'est pas accessible en écriture
-            dup2(sortie,1);
+            dup2(sortie, 1);
             close(sortie);
         }
     }
 
     if (!EST_EGAL(info->entree, "")) {  //redirection demandée
-        int entree = open(info->entree, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH );
+        int entree = open(info->entree, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
         if (entree != NULL) {   //si le fichier n'est pas accessible en écriture
-            dup2(entree,0);
+            dup2(entree, 0);
             close(entree);
         }
     }
 
-    for (int j = 0; j<nbArg; j++)
-        args[j] = info->ligne_cmd[debut+j];
-    args[nbArg] = (char *)0;
+    for (int j = 0; j < nbArg; j++)
+        args[j] = info->ligne_cmd[debut + j];
+    args[nbArg] = (char *) 0;
 
-
-
-    DEBUG(printf("Path : %s \n", p));
-    // TODO if ligne_cmd debut est ls on fait un execlp avec notre path
-    execvp(cmd, args);
-    exit(errno);
+    if (EST_EGAL(cmd, "ls")) {
+        execv("ls", args);
+        exit(errno);
+    }
+    else {
+        execvp(cmd, args);
+        exit(errno);
+    }
 }
